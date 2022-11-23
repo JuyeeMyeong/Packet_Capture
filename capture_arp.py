ebed7ceb0007e42c86c0dc1195e258fd8b699bc1
Juyee Myeong <93340211+JuyeeMyeong@users.noreply.github.com>
2022-11-23 18:02:07 +0900

    Update capture_arp.py

--- capture_arp.py
+++ capture_arp.py
@@ -4,41 +4,56 @@ from functools import reduce
 import binascii
 
 f = open('filenamehere.pcap', 'rb')
+#use pcap file reader to read packets
 pcap = dpkt.pcap.Reader(f).readpkts()
 
 def sep_string (addr):
     s = []
     for i in range(12//2):
+        #separate the MAC address and decode it into utf-8
         s.append(addr[i*2:i*2+2].decode('utf-8'))
+    #join all items into a string using ':' as a separator
     res_string = ":".join(s)
     return res_string
 
 arp_pktInfo = {"No." : 0, "Time" : 0.0, "Source": "", "Destination" : "", "Protocol" : "ARP", "Length" : 0}
+#the order number of packet
 pkt_num = 0
-res = []
+#timestamp when the first frame was captured
 first_frame = float(pcap[0][0]) 
 
 for packet in pcap:
+    #increment the order number by 1 for each packet
     pkt_num += 1
     arp_pktInfo["No."] = pkt_num
     e = dpkt.ethernet.Ethernet(packet[1])
     ip = e.data
     
+    #Check whether the packet type is ARP
     if e.type == dpkt.ethernet.ETH_TYPE_ARP:
         arp = e.arp
         ts = packet[0]
         
+        #Analyze MAC addresses
+        #e.src / e.dst: Ethernet or other MAC address for src and dst
         s_mac = sep_string(binascii.hexlify(e.src))
         d_mac = sep_string(binascii.hexlify(e.dst))
-        arp_pktInfo["Length"] = len(e)
-
-        src_ptc = socket.inet_ntoa(arp.spa)
-        src_hrd = sep_string(binascii.hexlify(arp.sha))
-        arp_pktInfo["Source"] = src_hrd
         
+        #Analyze "Length", "Source", "Destination", "Time", and IP addresses
+        #length of arp packet (in bytes)
+        arp_pktInfo["Length"] = len(e)
+        
+        #source protocol address
+        src_ptc = socket.inet_ntoa(arp.spa)
+        #src_hrd = sep_string(binascii.hexlify(arp.sha))
+        arp_pktInfo["Source"] = s_mac
+        
+        #target protocol address
         trg_ptc = socket.inet_ntoa(arp.tpa)
-        trg_hrd = sep_string(binascii.hexlify(arp.tha))
-        arp_pktInfo["Destination"] = trg_hrd
+        #trg_hrd = sep_string(binascii.hexlify(arp.tha))
+        arp_pktInfo["Destination"] = d_mac
+        
+        #seconds since first captured packet
         arp_pktInfo["Time"] = str(float(ts)  - first_frame)
 
         #IP address for request or reply
